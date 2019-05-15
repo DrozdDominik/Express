3a4f8bb6137b5dbccd7e84b6cb4a7f700e3ec44e
Dominik Drozd <dominik_drozd@vp.pl>
2019-05-15 11:31:03 +0200

    add connection to MongoDB

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    db: 'mongodb+srv://Dromind:SimplePassword@cluster0-eg9yf.mongodb.net/test?retryWrites=true',
     keySession: ['DROMIND'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
 
 router.all('*',  (req, res, next) => {
     if(!req.session.admin){
